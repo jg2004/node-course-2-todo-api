b3cef60490f4d29485260eb9170bd61f606d5926
jg2004 <jgdeveloper2004@gmail.com>
2018-07-25 19:50:12 -0400

    added GET

--- server/server.js
+++ server/server.js
@@ -2,27 +2,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoose = require('./db/mongoose');
-const {Todo}= require('./models/todo');
-const {User} = require('./models/user');
+const {
+  Todo
+} = require('./models/todo');
+const {
+  User
+} = require('./models/user');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+//POST a TODO
 app.post('/todos', (req, res) => {
 
-  console.log(req.body.text,"jerry!")
-  
   const todo = new Todo({
     text: req.body.text
   })
   todo.save().then((doc) => {
     res.send(doc)
   }, (e) => {
-
     res.status(400).send(e);
   })
 })
+
+//GET ALL TODOS
+app.get('/todos', (req, res) => {
+
+  Todo.find().then((todos) => {
+    res.send({todos})
+    console.log(todos);
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
-module.exports = {app}
+module.exports = {
+  app
+}
