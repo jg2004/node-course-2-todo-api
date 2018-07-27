2c9a0144ff4d26500528a36eff96c77f4b9f5672
jg2004 <jgdeveloper2004@gmail.com>
2018-07-27 07:12:40 -0400

    mlab uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
+//Had to create a free "sandbox" account with mlab. Set up a db user with user: jerry, pw:springer10 and use uri mlab provided. stored this uri into mLabUri variable to connect to mongodb database
+
 const mongoose = require('mongoose')
 
 const mLabUri = 'mongodb://jerry:springer10@ds029117.mlab.com:29117/todoapp';
-mongoose.connect(mLabUri, {
+const localHost = 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(mLabUri||localHost, {
   useNewUrlParser: true
 })
 // mongoose.connect('mongodb://localhost:27017/TodoApp', {
