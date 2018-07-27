a695d06bf2e158836756a816394cbc31cb3e2fed
jg2004 <jgdeveloper2004@gmail.com>
2018-07-27 08:09:22 -0400

    added process.env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,12 @@
 //Had to create a free "sandbox" account with mlab. Set up a db user with user: jerry, pw:springer10 and use uri mlab provided. stored this uri into mLabUri variable to connect to mongodb database
+//const mLabUri = 'mongodb://jerry:springer10@ds029117.mlab.com:29117/todoapp';
 
 const mongoose = require('mongoose')
-
-const mLabUri = 'mongodb://jerry:springer10@ds029117.mlab.com:29117/todoapp';
 const localHost = 'mongodb://localhost:27017/TodoApp';
-mongoose.connect(mLabUri||localHost, {
+mongoose.connect(process.env.MONGODB_URI||localHost, {
   useNewUrlParser: true
 })
-// mongoose.connect('mongodb://localhost:27017/TodoApp', {
-//   useNewUrlParser: true
-// })
+
 module.exports = {
   mongoose
 };
