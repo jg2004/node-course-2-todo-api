95001e4daa85c3d35aa615430152ff8194803755
jg2004 <jgdeveloper2004@gmail.com>
2018-07-27 06:57:00 -0400

    fixed mlab user pw

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
-const mLabUri = 'mongodb://<jerry>:<springer10>@ds029117.mlab.com:29117/todoapp';
-mongoose.connect(mLabUri || 'mongodb://localhost:27017/TodoApp', {
+const mLabUri = 'mongodb://jerry:springer10@ds029117.mlab.com:29117/todoapp';
+mongoose.connect(mLabUri, {
   useNewUrlParser: true
 })
-
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {
+//   useNewUrlParser: true
+// })
 module.exports = {
   mongoose
 };
