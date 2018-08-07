a4bebe612dcaed0925c83abf393d563ffc048ec0
jg2004 <jgdeveloper2004@gmail.com>
2018-08-06 20:11:24 -0400

    added delete

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config')
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const {
   ObjectID
@@ -13,7 +14,9 @@ const {
 const {
   User
 } = require('./models/user');
-const {authenticate} = require('./middleware/authenticate');
+const {
+  authenticate
+} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT;
@@ -148,6 +151,30 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+//POST /users/login {email,password)}
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user)
+      })
+    }).catch((e) => {
+      res.status(400).send();
+    })
+})
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send()
+  })
+})
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,20 @@
 const expect = require('expect');
 const request = require('supertest');
-const {ObjectID} = require('mongodb')
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
-const {User} = require('./../models/user');
+const {
+  ObjectID
+} = require('mongodb')
+const {
+  app
+} = require('./../server');
+const {
+  Todo
+} = require('./../models/todo');
+const {
+  User
+} = require('./../models/user');
 
-const {todos,
+const {
+  todos,
   populateTodos,
   users,
   populateUsers
@@ -219,30 +228,113 @@ describe('POST /users', () => {
         expect(res.body._id).toBeTruthy();
         expect(res.body.email).toBe(email);
       })
-      .end((err)=>{
-        if (err){return done(err);}
-        User.findOne({email}).then((user)=>{
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        User.findOne({
+          email
+        }).then((user) => {
           expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
-        })
+        }).catch((e) => done(e))
       })
   });
   it('should return validation erros if request invalid', (done) => {
 
     request(app)
-    .post('/users')
-    .send({email:'and',password:'123'})
-    .expect(400)
-    .end(done);
+      .post('/users')
+      .send({
+        email: 'and',
+        password: '123'
+      })
+      .expect(400)
+      .end(done);
 
   });
   it('should not create user if email in use', (done) => {
 
     request(app)
-    .post('/users')
-    .send({email: users[0].email,password:'Password123!'})
-    .expect(400)
-    .end(done)
+      .post('/users')
+      .send({
+        email: users[0].email,
+        password: 'Password123!'
+      })
+      .expect(400)
+      .end(done)
   });
 });
+describe('POST /users/login', () => {
+
+  it('should login user and return auth token', (done) => {
+
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[0]).toMatchObject({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+          }).catch((e) => done(e))
+      })
+  })
+
+
+  it('reject an invalid login', (done) => {
+
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: "abc"
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeFalsy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e) => done(e))
+      })
+  })
+
+})
+
+describe('DELETE /users/me/token', () => {
+
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err) => {
+        if(err){done(err)}
+        User.findById(users[0]._id)
+          .then(user => {``            
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e)=>done(e))
+      })
+  })
+})
